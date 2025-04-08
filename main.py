54f251e4ec07db6b350bd23d5175a29d2f3be067
bartlmac <206703800+bartlmac@users.noreply.github.com>
2025-04-08 15:10:18 +0200

    Update: Test mit micro Editor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def begruessung(name):
-    return f"Hallo {name}, willkommen bei GitHub und auf dem Raspberry Pi!"
+    return f"Hallo {name}, willkommen bei GitHub und in einer besseren Umgebung auf dem Raspberry Pi!"
 
 if __name__ == "__main__":
     print(begruessung("Bartłomiej"))
